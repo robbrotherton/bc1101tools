54412b0e40c49527baa41796c397a8d6d3e1c87f
robbrotherton <robert.brotherton@gmail.com>
2022-10-18 11:52:24 -0400

    add z-score example data

--- data-raw/z.R
+++ data-raw/z.R
@@ -1,6 +1,11 @@
 ## code to prepare `z` dataset goes here
 
-rt <- list(m = 284, sd = 50, x = 159)
-rt$z <- (rt$x  -rt$m) / rt$sd
+spiderman <- list(m = 284, sd = 50, x = 159)
+spiderman$z <- (rt$x  -rt$m) / rt$sd
 
-usethis::use_data(rt, overwrite = TRUE)
+usethis::use_data(spiderman, overwrite = TRUE)
+
+
+cbt <- list(m = 30.25, sd = 14.89, x = 15.49, n = 40)
+cbt$z <- (rt$x - rt$m) / rt$sd
+usethis::use_data(cbt, overwrite = TRUE)
