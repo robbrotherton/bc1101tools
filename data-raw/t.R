9b1669c51a0bcc80847606b9380b905d41689643
robbrotherton <robert.brotherton@gmail.com>
2022-10-26 10:05:13 -0400

    update data

---
+++ data-raw/t.R
@@ -0,0 +1,10 @@
+## code to prepare `t` dataset goes here
+
+class_reaction_times <- c(327, 335, 359, 430, 275.4,
+                          272, 350, 343.2, 278, 354,
+                          303, 328, 371, 312, 346,
+                          359, NA, 259, 313.6, 258,
+                          244, 374.4, NA, 338, 290)
+
+
+usethis::use_data(class_reaction_times, overwrite = TRUE)
