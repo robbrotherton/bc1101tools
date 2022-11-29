ffd40e7f35c07c2fa95cbbd8408b2195cc024574
robbrotherton <robert.brotherton@gmail.com>
2022-11-29 12:13:43 -0500

    add factorial anova test

--- tests/testthat/test-worked_examples.R
+++ tests/testthat/test-worked_examples.R
@@ -14,3 +14,20 @@ test_that("example ANOVA data generator works", {
   res <- example_ANOVA(c(9,11,13),c(3,5,4),c(5,6,7))
   expect_equal(res$f_ratio, 19.5)
 })
+
+test_that("factorial ANOVA data generator works", {
+
+  banana_math <- c(7,9,8,9)
+  banana_rt <- c(5,4,6,5)
+  candy_math <- c(5,3,4,4)
+  candy_rt <- c(6,6,5,5)
+
+  df <- data.frame(A = rep(c(1,2), each = 8),
+                   B = rep(c(1,2), each = 4),
+                   DV = c(banana_math, banana_rt, candy_math, candy_rt))
+
+  res <- bc1101tools::example_ANOVA_factorial(df)
+
+  expect_equal(res$f_ab, 34.94, tolerance = 0.001)
+
+})
